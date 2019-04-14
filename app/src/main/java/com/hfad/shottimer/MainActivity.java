38403fe34c82c919b97d5b0561ca11ddb18cab3b
cliford <ckr9756@uncw.edu>
2019-04-14 17:46:28 -0400

    Home Screen created with correct layout and icon placement (clickable). Necessary Activities created and start when clicked.

--- app/src/main/java/com/hfad/shottimer/MainActivity.java
+++ app/src/main/java/com/hfad/shottimer/MainActivity.java
@@ -1,7 +1,10 @@
 package com.hfad.shottimer;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().getDecorView().setBackgroundColor(Color.GRAY);
+    }
+
+    public void historicalAct(View view) {
+        Intent historical = new Intent(this, historicalActivity.class);
+        startActivity(historical);
+    }
+
+    public void timerActivity(View view) {
+        Intent historical = new Intent(this, timerActivity.class);
+        startActivity(historical);
+    }
+
+    public void statsActivity(View view) {
+        Intent historical = new Intent(this, statisticalActivity.class);
+        startActivity(historical);
+    }
+
+    public void questionActivity(View view) {
+        Intent historical = new Intent(this, questionActivity.class);
+        startActivity(historical);
     }
 }

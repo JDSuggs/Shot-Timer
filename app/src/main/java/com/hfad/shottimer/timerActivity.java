e6b2798dd846b78bfd65da4943a1871e837135bd
cliford <ckr9756@uncw.edu>
2019-04-15 08:26:15 -0400

    Making a push since the drawables did not update name.

--- app/src/main/java/com/hfad/shottimer/timerActivity.java
+++ app/src/main/java/com/hfad/shottimer/timerActivity.java
@@ -12,6 +12,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -105,6 +106,7 @@ public class timerActivity extends AppCompatActivity {
                     }
 
                 }
+
             });
         }else{
             requestPermissions();
@@ -117,6 +119,8 @@ public class timerActivity extends AppCompatActivity {
         mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
         mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
         mediaRecorder.setOutputFile(pathSaved);
+        TextView text = findViewById(R.id.pathSaved);
+        text.setText(this.pathSaved);
     }
 
     private void requestPermissions() {
